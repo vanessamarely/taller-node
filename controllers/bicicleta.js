adc38619a0cf15df1d836d52bf482a2f888b1bc2
Vanessa Aristizabal <vanessamarely@gmail.com>
2023-10-19 21:40:35 -0500

    Update bicicleta.js

--- controllers/bicicleta.js
+++ controllers/bicicleta.js
@@ -28,3 +28,24 @@ exports.delete = function (req, res) {
 
   res.redirect("/bicicletas");
 };
+
+exports.update_get = function (req, res) {
+  res.render("bicicletas/create");
+};
+
+exports.update_post = function (req, res) {
+  var bici = Bicicleta.findById(req.params.id);
+
+  bici.id = req.body.id;
+  bici.color = req.body.color;
+  bici.modelo = req.body.modelo;
+  bici.ubicacion = [req.body.lat, req.body.lng];
+
+  res.redirect("/bicicletas");
+};
+
+exports.update_get = function (req, res) {
+  var bici = Bicicleta.findById(req.params.id);
+
+  res.render("bicicletas/update", { bici });
+};
